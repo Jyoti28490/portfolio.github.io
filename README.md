181f3424f845b2d9bdc3be833ac4d2c5528a1fa0
Jyoti28490 <Jyotirmayi.swain@gmail.com>
2021-04-26 15:54:48 +0530

    Added my project

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # React-portfolio
 
-A boilrplate react portfolio to showcase your projects and work
+A boilerplate react portfolio to showcase your projects and work
 
+<<<<<<< HEAD
 
 Dummy-portfolio link -> https://jyoti28490.github.io/portfolio.github.io/
+=======
+<!-- Dummy-portfolio link -> https://shloksomani.github.io/react-portfolio/ -->
+>>>>>>> 2e53040... after g-pages installation
 
 ![](test.gif)
---
+++ build/precache-manifest.0f293d9bffe450c1192f9aa2e115dacf.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c0de55be83f97c8f95236ac0916a62",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3c6c9e5bfd1599f2c7c9",
+    "url": "/react-portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "b6d2f8be7e702a31bba2",
+    "url": "/react-portfolio/static/css/main.926de77e.chunk.css"
+  },
+  {
+    "revision": "3c6c9e5bfd1599f2c7c9",
+    "url": "/react-portfolio/static/js/2.bcd9edf7.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/react-portfolio/static/js/2.bcd9edf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6d2f8be7e702a31bba2",
+    "url": "/react-portfolio/static/js/main.81be739d.chunk.js"
+  },
+  {
+    "revision": "d0f621f0c631a2c92503",
+    "url": "/react-portfolio/static/js/runtime-main.0c4e4ecc.js"
+  },
+  {
+    "revision": "5523e1a934f130fb012de52e6718ce2f",
+    "url": "/react-portfolio/static/media/Airbnb_1.5523e1a9.PNG"
+  },
+  {
+    "revision": "b013ebea0cf8c072f7bbc40e27a67aed",
+    "url": "/react-portfolio/static/media/Airbnb_2.b013ebea.PNG"
+  },
+  {
+    "revision": "1e311187d705e72aea4581f8c5eacc06",
+    "url": "/react-portfolio/static/media/Airbnb_3.1e311187.PNG"
+  },
+  {
+    "revision": "5f1d1625ec70a52579b45314d6af224c",
+    "url": "/react-portfolio/static/media/Netflix_Clone_1.5f1d1625.PNG"
+  },
+  {
+    "revision": "a1c87eb4285b9e6486815fe1b6ebb1ed",
+    "url": "/react-portfolio/static/media/Netflix_Clone_2.a1c87eb4.PNG"
+  },
+  {
+    "revision": "9bfde20b27b3154108a8bf3d85113f37",
+    "url": "/react-portfolio/static/media/Netflix_Clone_3.9bfde20b.PNG"
+  },
+  {
+    "revision": "75ff1c25fda8d8d180a6b3a1c2e32272",
+    "url": "/react-portfolio/static/media/React_Resp_1.75ff1c25.PNG"
+  },
+  {
+    "revision": "bc437e2179003c5bccf5c803f2ce3fea",
+    "url": "/react-portfolio/static/media/React_Resp_2.bc437e21.PNG"
+  },
+  {
+    "revision": "ea703cfb1350d81849ff1e2de407b9fd",
+    "url": "/react-portfolio/static/media/React_Resp_3.ea703cfb.PNG"
+  },
+  {
+    "revision": "f7d0a4eeeb6acc22bb8942ed48d3620f",
+    "url": "/react-portfolio/static/media/React_Resp_4.f7d0a4ee.PNG"
+  },
+  {
+    "revision": "599c6315d2ee314ecc28c54ce1c6cd53",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_1.599c6315.PNG"
+  },
+  {
+    "revision": "fe4cc2b319444e1a787eb843cbbc90c2",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_2.fe4cc2b3.PNG"
+  },
+  {
+    "revision": "41b2e47eeefcec2d8a559758decf7eed",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_3.41b2e47e.PNG"
+  },
+  {
+    "revision": "a02af0719b9ff153e811d1b38cdbba68",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_4.a02af071.PNG"
+  },
+  {
+    "revision": "7bd1a08dfe121b499a0b10e9b8a2e7e0",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_5.7bd1a08d.PNG"
+  },
+  {
+    "revision": "f7eba4b968850ec4f98042c3b5560050",
+    "url": "/react-portfolio/static/media/React_Smooth_Scroll_6.f7eba4b9.PNG"
+  },
+  {
+    "revision": "40f28369e42a68148dff9a530faf54f5",
+    "url": "/react-portfolio/static/media/Recipe_App_1.40f28369.PNG"
+  },
+  {
+    "revision": "460f6335deaf251e7d62afd70a6066d5",
+    "url": "/react-portfolio/static/media/Recipe_App_2.460f6335.PNG"
+  },
+  {
+    "revision": "15a9dc0064af27030281ec8bced3d1fb",
+    "url": "/react-portfolio/static/media/Recipe_App_3.15a9dc00.PNG"
+  },
+  {
+    "revision": "d26c5cf74fe6df8fd4f593a61e46e0c4",
+    "url": "/react-portfolio/static/media/Weather_1.d26c5cf7.PNG"
+  },
+  {
+    "revision": "5de43becacfafd5b026329fcbd778422",
+    "url": "/react-portfolio/static/media/Weather_2.5de43bec.PNG"
+  },
+  {
+    "revision": "f1c6df876347a145ba3e2a5ea24de10f",
+    "url": "/react-portfolio/static/media/Weather_3.f1c6df87.PNG"
+  },
+  {
+    "revision": "c328f6111c94f0070e3fbfefe4a9b166",
+    "url": "/react-portfolio/static/media/Weather_4.c328f611.PNG"
+  },
+  {
+    "revision": "153653f4accd9a20e885f3ee90aca8c7",
+    "url": "/react-portfolio/static/media/Weather_5.153653f4.PNG"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/react-portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/react-portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/react-portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/react-portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/react-portfolio/static/media/ionicons.c037dbbc.svg"
+  }
+]);
